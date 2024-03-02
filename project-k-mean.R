1fd8d4bd485ce66e124daead81478781c399281d
namratadakua <namratadakua@users.noreply.github.com>
2024-03-02 13:04:36 +0000

    Add files via upload

--- project-k-mean.R
+++ project-k-mean.R
@@ -6,9 +6,11 @@ library(kableExtra)
 library(factoextra)
 
 portfolio <- stream_in(file("portfolio.json"))
-profile <- stream_in(file("./profile.json"))
+profile <- stream_in(file("profile.json"))
 
-transcript <- stream_in(file("./transcript.json"))
+profile$profile_number <- seq(from =1 , to=nrow(profile))
+
+transcript <- stream_in(file("transcript.json"))
 transactions <- data.frame(
   person_id = transcript[["person"]],
   event = transcript[["event"]],
@@ -19,28 +21,37 @@ transactions <- data.frame(
   time = transcript[["time"]]
 )
 
-transaction_offer_completed <- subset(transactions, event == "offer completed")
+transaction_with_profile <- left_join(transactions, profile, by = c("person_id" = "id"))
+
+
+transaction_offer_completed <- subset(transaction_with_profile, event == "offer completed")
 
 transactions_by_person_event <- transaction_offer_completed %>%
-  group_by(person_id) %>%
+  group_by(profile_number) %>%
   summarize(
-    total_reward_paid = sum(reward_paid),
-    avg_time = mean(time),
-    total_offer_id = n()
+    Monetary = sum(reward_paid),
+    Recency = mean(time),
+    Frequency = n()
     # You can add more aggregations for other columns if needed
   )
 
 transactions_by_person_event <- as.data.frame(transactions_by_person_event)
 
 # Assign 'ID' column as row names
-rownames(transactions_by_person_event) <- transactions_by_person_event$person_id
+#rownames(transactions_by_person_event) <- transactions_by_person_event$profile_number
 
 # Remove the 'ID' column if necessary
-transactions_by_person_event <- transactions_by_person_event[, -which(names(transactions_by_person_event) == "person_id")]
+#transactions_by_person_event <- transactions_by_person_event[, -which(names(transactions_by_person_event) == "profile_number")]
+
+#transactions_by_person_event$total_reward_paid = as.numeric(transactions_by_person_event$total_reward_paid)
+#transactions_by_person_event$total_offer_id = as.numeric(transactions_by_person_event$total_offer_id)
+
+standardized_txn_data <- scale(transactions_by_person_event[, c("Recency", "Frequency", "Monetary")])
+standardized_txn_data
 
 wss <- numeric(10)
 for (i in 1:10) {
-  kmeans_model <- kmeans(transactions_by_person_event, centers = i)
+  kmeans_model <- kmeans(standardized_txn_data, centers = i)
   wss[i] <- sum(kmeans_model$withinss)
 }
 
@@ -50,24 +61,26 @@ plot(1:10, wss, type = "b", pch = 19, frame = FALSE, xlab = "Number of Clusters"
 
 # Apply k-means clustering with the chosen number of clusters
 k <- 4  # Choose the number of clusters based on the elbow plot
-kmeans_model <- kmeans(transactions_by_person_event, centers = k, nstart = 25)
+kmeans_model <- kmeans(standardized_txn_data, centers = k, nstart = 25)
 
 # Add cluster assignment to the original dataset
 transactions_by_person_event$Cluster <- as.factor(kmeans_model$cluster)
 
 # Display the first few rows of the dataset with cluster assignments
-print(head(transactions_by_person_event))
+print(head(standardized_txn_data))
 
 # Visualize the clusters in a scatter plot
-fviz_cluster(kmeans_model, data = transactions_by_person_event, geom = "point", frame.type = "norm")
+fviz_cluster(kmeans_model, data = standardized_txn_data, geom = "point", ellipse = "norm")
 
+fviz_cluster(kmeans_model, data = standardized_txn_data,  ellipse = "norm") 
+transactions_by_person_event <- transactions_by_person_event[, -which(names(transactions_by_person_event) == "profile_number")]
 # Display summary statistics for each cluster
 cluster_summary <- transactions_by_person_event %>%
   group_by(Cluster) %>%
   summarize(
-    Avg_Recency = mean(avg_time),
-    Avg_Frequency = mean(total_offer_id),
-    Avg_Monetary = mean(total_reward_paid),
+    Avg_Recency = mean(Recency),
+    Avg_Frequency = mean(Frequency),
+    Avg_Monetary = mean(Monetary),
     Count = n()
   )
 
